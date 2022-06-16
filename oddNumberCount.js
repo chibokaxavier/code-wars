13b21f2003e152e261e4f0fa99f99ee34c20a59c
Xavier <chibokaxavier@gmail.com>
2022-06-16 15:45:57 +0100

    first commit

---
+++ oddNumberCount.js
@@ -0,0 +1,9 @@
+function oddCount(n){
+    let total = 0
+  for ( i=0; i<n; i++) {
+    if (i%2 !=0)
+      total++
+  }
+    return total
+  }
+  
---
+++ squrenumbertheadd.js
@@ -0,0 +1,16 @@
+function squareSum(numbers) {
+    let total = 0 
+    // for (let i = 0; < SVGAnimatedNumberList.length; i++) {
+    //     total+= Math.pow(numbers[i],2);
+   
+    // }
+    // for (let num of numbers){
+    //     total += Math.pow(num,2)
+    // }
+    // numbers.array.forEach(num => {
+    //     total += Math.pow(num,2)
+        
+    // });
+    // numbers.map(num => total +=Math.pow(num,2))
+    // return total
+}
